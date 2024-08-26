afbb65c034eb799e52a179fa02bd62d2cf559bad
Joao Henrique <joaohenriquefsf@gmail.com>
2024-08-26 13:13:50 -0300

    feat: add model to django admin

--- backend/fibonacci/admin.py
+++ backend/fibonacci/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from .models import FibonacciData
 
 # Register your models here.
+@admin.register(FibonacciData)
+class FibonacciDataAdmin(admin.ModelAdmin):
+    list_display = ('value', 'serie')
